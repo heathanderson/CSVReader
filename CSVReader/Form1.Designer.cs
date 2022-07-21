7043e84be1e2928b431e29689569f97e868d0002
Heath Anderson <heath.anderson@superconcepts.com.au>
2022-07-21 21:56:49 +1000

    Initial Version

---
+++ CSVReader/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace CSVReader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtFile = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnFile1 = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.ChkAutoRefresh = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtFile
+            // 
+            this.txtFile.AllowDrop = true;
+            this.txtFile.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtFile.BackColor = System.Drawing.Color.White;
+            this.txtFile.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtFile.Location = new System.Drawing.Point(12, 11);
+            this.txtFile.Name = "txtFile";
+            this.txtFile.Size = new System.Drawing.Size(365, 20);
+            this.txtFile.TabIndex = 1;
+            this.txtFile.DragDrop += new System.Windows.Forms.DragEventHandler(this.txtFile_DragDrop);
+            this.txtFile.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtFile_KeyUp);
+            this.txtFile.DragEnter += new System.Windows.Forms.DragEventHandler(this.txtFile_DragEnter);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowDrop = true;
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.CausesValidation = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(1, 39);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(595, 358);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.DragEnter += new System.Windows.Forms.DragEventHandler(this.txtFile_DragEnter);
+            this.dataGridView1.DragDrop += new System.Windows.Forms.DragEventHandler(this.txtFile_DragDrop);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnLoad.Location = new System.Drawing.Point(415, 9);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 3;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnFile1
+            // 
+            this.btnFile1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnFile1.Location = new System.Drawing.Point(383, 9);
+            this.btnFile1.Name = "btnFile1";
+            this.btnFile1.Size = new System.Drawing.Size(26, 23);
+            this.btnFile1.TabIndex = 4;
+            this.btnFile1.Text = "...";
+            this.btnFile1.UseVisualStyleBackColor = true;
+            this.btnFile1.Click += new System.EventHandler(this.btnFile1_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.Filter = "CSV files|*.csv|Text Files|*.txt|All files|*.*";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // ChkAutoRefresh
+            // 
+            this.ChkAutoRefresh.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ChkAutoRefresh.AutoSize = true;
+            this.ChkAutoRefresh.Location = new System.Drawing.Point(496, 13);
+            this.ChkAutoRefresh.Name = "ChkAutoRefresh";
+            this.ChkAutoRefresh.Size = new System.Drawing.Size(88, 17);
+            this.ChkAutoRefresh.TabIndex = 5;
+            this.ChkAutoRefresh.Text = "Auto Refresh";
+            this.ChkAutoRefresh.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(596, 397);
+            this.Controls.Add(this.ChkAutoRefresh);
+            this.Controls.Add(this.btnFile1);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtFile);
+            this.Name = "Form1";
+            this.Text = "CSV Viewer";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFile;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnFile1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.CheckBox ChkAutoRefresh;
+    }
+}
+
---
+++ CSVReader/Form1.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CSVReader
+{
+    public partial class Form1 : Form
+    {
+        private string[] alphabet = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+        private char splitChar = '^';
+        private bool fileLoaded = false;
+        private FileInfo fi;
+        private DataTable dt1 = null;
+        private DataTable dt2 = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public Form1(string filename)
+        {
+            InitializeComponent();
+            txtFile.Text = filename;
+            loadFile();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            loadFile();
+        }
+
+        private bool loadFile()
+        {
+            int numColumns = 0;
+            int rowCount = 0;
+
+            dataGridView1.Columns.Clear();
+
+            try
+            {
+                using (TextReader reader = new StreamReader(txtFile.Text))
+                {
+                    fi = new FileInfo(txtFile.Text);
+
+                    string line;
+                    if (dt1 != null)
+                        dt2 = dt1.Copy();
+
+                    dt1 = new DataTable();
+
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        rowCount++;
+
+                        string[] lineArray = split(line);
+
+                        for (int i = numColumns; i < lineArray.Length; i++)
+                        {
+                            numColumns++;
+                            string colName = getColumnName(numColumns);
+                            dt1.Columns.Add(colName);
+                        }
+
+                        dt1.Rows.Add(lineArray);
+                    }
+                }
+                LoadTable();
+            }
+            catch (Exception ex)
+            {
+            }
+
+            return true;
+        }
+
+        private string getColumnName(int i)
+        {
+            string name = (i + 1) + ":";
+
+            int factor = i / alphabet.Length - 1;
+
+            if (factor >= 0)
+            {
+                name += alphabet[factor];
+            }
+            name += alphabet[i % alphabet.Length];
+
+            return name;
+        }
+
+        private string[] split(string line)
+        {
+            string[] lineArray;
+            string newLine = "";
+            bool inString = false;
+
+            if (line == null)
+                return new string[0];
+
+            for (int i = 0; i < line.Length; i++)
+            {
+                if (line[i] == '"')
+                {
+                    inString = !inString;
+                    if (i < line.Length - 1 && line[i + 1] == '"')
+                    {
+                        newLine += line[i];
+                    }
+                }
+                else if (!inString && line[i] == ',')
+                {
+                    newLine += splitChar;
+
+                }
+                else
+                {
+                    newLine += line[i];
+                }
+            }
+            lineArray = newLine.Split(splitChar);
+            return lineArray;
+        }
+
+        private void LoadTable()
+        {
+            for (int i = 0; i < dt1.Columns.Count; i++)
+            {
+                string colName = getColumnName(i);
+                dataGridView1.Columns.Add(colName, colName);
+            }
+
+            for (int rowNum = 0; rowNum < dt1.Rows.Count; rowNum++)
+            {
+                DataRow dr1;
+                DataRow dr2;
+
+                dataGridView1.Rows.Add();
+
+                dr1 = dt1.Rows[rowNum];
+
+                if (dt2 != null && rowNum <= dt2.Rows.Count)
+                    dr2 = dt2.Rows[rowNum];
+                else
+                    dr2 = null;
+
+                if (rowNum % 2 == 0)
+                    dataGridView1.Rows[rowNum].DefaultCellStyle.BackColor = Color.LightBlue;
+                else
+                    dataGridView1.Rows[rowNum].DefaultCellStyle.BackColor = Color.LightCyan;
+
+                for (int colNum = 0; colNum < dt1.Columns.Count; colNum++)
+                {
+                    try
+                    {
+                        dataGridView1.Rows[rowNum].Cells[colNum].Value = dr1[colNum].ToString();
+
+                        if (
+                            dr2 != null
+                            && colNum <= dt2.Columns.Count
+                            && !dr2[colNum].Equals(dr1[colNum])
+                            )
+                        {
+                            dataGridView1.Rows[rowNum].Cells[colNum].ToolTipText = dr2[colNum].ToString();
+                            dataGridView1.Rows[rowNum].Cells[colNum].Style.BackColor = Color.Red;
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                    }
+                }
+            }
+        }
+
+
+        #region Events (Button Clicks etc...)
+        private void txtFile_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                loadFile();
+        }
+
+        private void btnFile1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                txtFile.Text = openFileDialog.FileName;
+            }
+        }
+        #endregion
+
+        #region Drag and Drop stuff
+        private void txtFile_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.Text))
+                txtFile.Text = e.Data.GetData(DataFormats.Text).ToString();
+            else if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] s = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+                txtFile.Text = s[0];
+                txtFile.Focus();
+                this.Activate();
+                loadFile();
+            }
+        }
+
+        private void txtFile_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.Text))
+                e.Effect = DragDropEffects.Copy;
+            else if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.All;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+        #endregion
+
+        #region Timer
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            if (ChkAutoRefresh.Checked)
+            {
+                try
+                {
+                    FileInfo f = new FileInfo(txtFile.Text);
+                    if (f.LastWriteTimeUtc != fi.LastWriteTimeUtc)
+                        loadFile();
+                }
+                catch (Exception ex) { }
+            }
+        }
+        #endregion
+
+    }
+}
